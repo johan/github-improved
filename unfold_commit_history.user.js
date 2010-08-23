1f571598a5fe4a1e7fce5ca25ae93c5f5cfe0c4d
Johan Sundström <oyasumi@gmail.com>
2010-08-23 01:00:00 -0700

    Lots of fixes:
     * don't just run on the first page of commits
     * improved docs
     * show loading throbber where the timestamps are
     * prettified "(un)fold all commits" link
     * gave it a hotkey "f"
     * fully compatible with stuff like http://autopagerize.net/
     * don't need to unfold all before clicking individual changesets

--- unfold_commit_history.user.js
+++ unfold_commit_history.user.js
@@ -1,50 +1,85 @@
 // ==UserScript==
 // @name          Github: unfold commit history
 // @namespace     http://github.com/johan/
-// @description   Adds an "unfold all changesets" button on Commit History pages at github, which lets you browse the source changes without leaving the page. (Click a commit header again to re-fold it.)
-// @include       https://github.com/*/commits/*
-// @include       http://github.com/*/commits/*
+// @description   Adds "unfold all changesets" buttons (hotkey: f) above/below Commit History pages at github, letting you browse the source changes without leaving the page. (Click a commit header again to re-fold it.) You can also fold or unfold individual commits by clicking on non-link parts of the commit.
+// @include       https://github.com/*/commits*
+// @include       http://github.com/*/commits*
 // ==/UserScript==
 
 var css = // used for .toggleClass('folded'), for, optionally, hiding:
   '.file.folded > .data,\n' + // individual .commit .changeset .file:s
+  '.file.folded > .image,\n' + // (...or their corresponding .image:s)
   '.commit.folded .changeset,\n' + // whole .commit:s' diffs,
   '.commit.folded .message .full' + // + full checkin message
-  ' { display: none; }';
+  ' { display: none; }\n' +
+  '.commit.loading .actor .date:before\n' + // show a loading throbber
+  ' { content: url("/images/modules/browser/loading.gif"); }\n' +
+  '.commit.loading .actor .date abbr { visibility: hidden; }\n' +
+  '.fold_unfold { float: right; }\n' +
+  '.all_folded .fold_unfold:before { content: "\xAB un"; }\n' +
+  '.all_folded .fold_unfold:after { content: " \xBB"; }\n' +
+  '.all_unfolded .fold_unfold:before { content: "\xBB "; }\n' +
+  '.all_unfolded .fold_unfold:after { content: " \xAB"; }\n';
 
-(function() {
-  if ('undefined' == typeof __PAGE_SCOPE_RUN__) { // unsandbox, please!
+// This block of code injects our source in the content scope and then calls the
+// passed callback there. The whole script runs in both GM and page content, but
+// since we have no other code that does anything, the Greasemonkey sandbox does
+// nothing at all when it has spawned the page script, which gets to use jQuery.
+// (jQuery unfortunately degrades much when run in Mozilla's javascript sandbox)
+(function(run_me_in_page_scope) {
+  if ('undefined' == typeof __RUNS_IN_PAGE_SCOPE__) { // unsandbox, please!
     var src = arguments.callee.caller.toString(),
      script = document.createElement('script');
     script.setAttribute("type", "application/javascript");
-    script.innerHTML = "const __PAGE_SCOPE_RUN__ = true;\n(" + src + ')();';
+    script.innerHTML = "const __RUNS_IN_PAGE_SCOPE__ = true;\n(" + src + ')();';
     document.documentElement.appendChild(script);
     document.documentElement.removeChild(script);
   } else { // unsandboxed -- here we go!
-    $('head').append($('<style type="text/css"></style>').html(css));
-    $('<a class="fold_unfold" style="float: right;">unfold all changesets</a>')
-      .appendTo('#path').toggle(unfold, fold);
+    run_me_in_page_scope();
   }
-})();
+})(init);
 
-function unfold() {
-  var changesets = $('.changeset');
-  if (changesets.length)
-    changesets.parent().removeClass('folded');
+function init() {
+  $('body').addClass('all_folded');
+  $('head').append($('<style type="text/css"></style>').html(css));
+  $('.commit').live('click', toggle_commit_folding);
+
+  $('<div class="pagination" style="margin: 0; padding: 0;"></div>')
+    .prependTo('#commit .separator:first');
+  $('<a class="fold_unfold" hotkey="f">fold all changesets</a>')
+    .appendTo('.pagination');
+  $('.fold_unfold').toggle(unfold_all, fold_all);
+  window.toggle_all_folding = toggle_all_folding;
+  $.hotkey('f', 'javascript:void(toggle_all_folding())');
+}
+
+function toggle_all_folding() {
+  if ($('body').hasClass('all_folded'))
+    unfold_all();
   else
-    $('.envelope.commit .message a').each(inline);
-  this.textContent = 'fold all changesets';
+    fold_all();
 }
 
-function fold() {
-  $('.changeset').parent().addClass('folded');
-  this.textContent = 'unfold all changesets';
+function unfold_all() {
+  $('body').addClass('all_unfolded').removeClass('all_folded');
+  $('.commit.folded').removeClass('folded');
+  $('.envelope.commit .message a:not(.loaded)').each(inline_changeset);
 }
 
-function toggle_changeset(e) {
+function fold_all() {
+  $('body').addClass('all_folded').removeClass('all_unfolded');
+  $('.commit').addClass('folded');
+}
+
+function toggle_commit_folding(e) {
   if (isNotLeftButton(e) || $(e.target).closest('a, .changeset').length)
-    return; // clicked a link, or in the changeset
-  $('.changeset', this).parent().toggleClass('folded');
+    return; // clicked a link, or in the changeset; don't do fold action
+
+  var $link = $('.message a', this);
+  if ($link.hasClass('loaded'))
+    $(this).toggleClass('folded');
+  else
+    $link.each(inline_changeset);
 }
 
 // every mouse click is not interesting; return true only on left mouse clicks
@@ -53,7 +88,9 @@ function isNotLeftButton(e) {
   return (e.which > 1) || e.shiftKey || e.ctrlKey || e.altKey || e.metaKey;
 }
 
-function inline() {
+// loads the changeset link's full commit message, toc and the files changed and
+// inlines them in the corresponding changeset (in the current page)
+function inline_changeset() {
   // make file header click toggle showing file contents (except links @ right)
   function toggle_file(e) {
     if (isNotLeftButton(e) || $(e.target).closest('.actions').length)
@@ -81,18 +118,22 @@ function inline() {
     commit.attr('title', 'Touched '+ count +' file'+ (count == 1 ? '' : 's'));
 
     // now, add lines 2.. of the commit message to the unfolded changeset view
+      try {
     var whole = $('#commit', changeset), // contains the whole commit message
         line2 = $('.message pre', whole).html().replace(line1, ''),
         $span = $('<span class="full"></span>').html(line2);
     whole.remove(); // and remove the remaining duplicate parts of that commit
     $('.human .message pre', commit).append($span); // <pre> of commit message
+    commit.removeClass('loading');
+      }catch(e){
+          console.warn('error?', e);
+      }
   }
 
   var line1  = /^[^\n]*/,
       sha1   = this.pathname.slice(this.pathname.lastIndexOf('/') + 1),
-      commit = $(this)
-    .closest('.commit')
-    .click(toggle_changeset);
+      commit = $(this).closest('.commit').addClass('loading');
+  $(this).addClass('loaded'); // mark that we already did load it on this page
   commit.find('.human, .machine')
     .css('cursor', 'pointer');
   var changeset = commit
